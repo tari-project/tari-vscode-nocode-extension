254e4fad95897fdb883faffcb7e9e8465b025ffa
Stan Bondi <sdbondi@users.noreply.github.com>
2025-07-11 12:48:04 +0200

    fix bigin not serializable as json

--- packages/tari-extension-query-builder/src/types.ts
+++ packages/tari-extension-query-builder/src/types.ts
@@ -5,3 +5,20 @@ export interface TariFlowNodeDetails {
   templateAddress: string;
   functionName: string;
 }
+
+// Extend BigInt to support JSON serialization
+declare global {
+  interface BigInt {
+    toJSON(): string | number;
+  }
+}
+
+BigInt.prototype.toJSON = function () {
+  // if the number is too large, return it as a string
+  // otherwise return it as a number
+  const int = this.valueOf();
+  if (int > BigInt(Number.MAX_SAFE_INTEGER) || int < BigInt(Number.MIN_SAFE_INTEGER)) {
+    return this.toString();
+  }
+  return Number(this);
+};
